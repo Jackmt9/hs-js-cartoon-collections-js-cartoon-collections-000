48a659445c44a83af7ddf436c4b70ea82df2e1ad
dandy-clone-9171 <jackmt9@gmail.com>
2020-01-03 20:58:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function dwarfRollCall(dwarves) {
+  var dwarvesList = "";
+  for (var i = 0; i < dwarves.length; i++) {
+    var position = i + 1;
+    dwarvesList += position + ". " + dwarves[i] + " ";
+  }
+  return dwarvesList;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i = 0; i < planeteerCalls.length; i++){
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
+  }
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      return true;
+    }
+    else return false;
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++){
+    if (foods[i]=="cheddar" || foods[i]=="gouda" || foods[i]=="camembert"){
+      return foods[i];
+    }
+    else return "no cheese!";
+  }
 }
